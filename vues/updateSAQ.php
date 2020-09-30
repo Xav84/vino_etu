96f3320ed205a3530cb1d30b2fa3b75a75668307
Xavier Gratton <xavier_gratton@hotmail.com>
2020-09-30 16:50:59 -0400

    ajout de updateSAQ en temps que vue pour l importation

---
+++ vues/updateSAQ.php
@@ -0,0 +1,32 @@
+<h2>Importation de bouteilles de la SAQ</h2>
+
+<form id="importer" method="post">
+    <div class="importerBouteilles" vertical layout>
+
+        <input type="number" class="nombre_bouteille" name="nombre_bouteille" placeholder="24, 48 ou 96 " value="">
+        <button type="submit" name="importer" value=""><i class="far fa-arrow-alt-circle-down"></i></button>
+        <ul class="listeImportation">
+        </ul>
+    </div>
+</form>
+<?php
+
+$page = 1;
+
+if (isset($_POST['importer'])) {
+    if (preg_match('/^((24||48||96))$/', $_POST['nombre_bouteille'])) {
+
+        $nombreProduit = $_POST['nombre_bouteille']; //24, 48 ou 96	
+
+        $saq = new SAQ();
+
+        for ($i = 1; $i < 2; $i++)    //permet d'importer séquentiellement plusieurs pages.
+        {
+            echo "<h2>page " . ($page) . "</h2>";
+            $page++;
+            $nombre = $saq->getProduits($nombreProduit, $page + $i);
+            echo "importation : " . $nombre . "<br>";
+        }
+    } else echo "<h2 class='erreur'>Veuillez entrer les valeurs :24, 48 ou 96";
+}
+?>
